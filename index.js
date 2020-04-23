27b5eb774ea682853a0b05b67c166ca562a56858
Zinchenko <zinchenko@MacBook-Pro-Zinchenko.local>
2020-04-23 19:08:23 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
-let a = 1;
-const b = '2';
+// 1) Оголосіть три різні змінні за допомогою "let", "const", "var"
+let user = 'Victor';
+let age = 28;
+let surname = 'Zinchenko';
+
+let user = 'Victor', age = 28, surname = 'Zinchenko';
+
+const BIRTHDAY = '18.04.1982'
+const PI = 3.14
+const APPLE = 'the_best'
+
+var x = 2;
+var y = 3;
+var sum = x + y;
+
+// 2) Оголосіть одну змінну якій можна міняти значення і одну змінну якій не можна міняти значення
+
+let user = 'Victor';
+const APPLE = 'the_best'
+
+// 3) Напишіть 1 коментар в 1 рядок і напишіть ще один коментар на 4 рядки
+
+//Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+/*
+Lorem incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non.
+*/
+
+// 4) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу String.
+// * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+
+// 5) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу Number.
+// * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+
+// 6) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу Boolean.
+// * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+
+// 7) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу Object яке буде містити хоча б 3 ключі.
+// * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+
+// 8) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу Array яке буде містити хоча б 3 значення.
+// * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+
+// 9) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу Function.
+// * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+
+// 10) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу Undefined.
+// * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+
+// 11) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу Null.
+// * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+
+// 12) Оголосіть змінну user типу Object і опишіть своє: ім'я, фамілію, дату народження,
+// місце проживання, стать і можете добавити ще додаткові параметри використовуючи різні типи даних.
