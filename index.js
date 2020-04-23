8f55fa8ae7e90d275dca12544377fa012d4b1993
Zinchenko <zinchenko@MacBook-Pro-Zinchenko.local>
2020-04-23 21:39:31 +0300

    index.js

--- index.js
+++ index.js
@@ -30,15 +30,32 @@ cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non.
 
 // 4) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу String.
 // * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+let string = 'string'
+typeof 'string'
+console.log(typeof 'string');
 
 // 5) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу Number.
 // * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+let number = 23
+typeof 23
+console.log(typeof 23);
 
 // 6) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу Boolean.
 // * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+let green = true;
+let red = false;
+typeof true
+console.log(typeof true)
 
 // 7) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу Object яке буде містити хоча б 3 ключі.
 // * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
+let user1 = {}
+let user1 = { name: 'Victor', age: 28, location: 'Ireland' }
+
+object.keys(user1)
+typeof { a: 1 }
+console.log(typeof user1)
+
 
 // 8) Оголосіть одну змінну і прийсвойте в неї будь яке значення типу Array яке буде містити хоча б 3 значення.
 // * Bиведіть в консоль тип змінної виклристовуючи оператор "typeof" і функцію "console.log()"
